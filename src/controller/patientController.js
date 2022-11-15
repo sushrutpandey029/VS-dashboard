ff3648ac8b649d7428b15e9af5e85d84bbc86893
sushrutpandey <sushrutpandey1@gmail.com>
2022-11-15 10:34:47 +0530

    make it better

--- src/controller/patientController.js
+++ src/controller/patientController.js
@@ -355,7 +355,7 @@ const gamelist = async (req, res) => {
             return res.status(400).send({ status: false, msg: "please enter gamecategories to find the game" })
         }
 
-        const game = await gameModel.findAll({ gamecategories: gamecategories }).select({ _id: 1, gamecategories: 1, gamename: 1, gamedescription: 1, gamelink: 1})
+        const game = await gameModel.find({ gamecategories: gamecategories }).select({ _id: 1, gamecategories: 1, gamename: 1, gamedescription: 1, gamelink: 1})
 
         if (!game) {
             return res.status(400).send({ status: false, msg: "game not found please enter valid categories" })
