7bcedb3bbae97913164a12be5500654e9db927de
sushrutpandey <sushrutpandey1@gmail.com>
2022-11-18 13:26:14 +0530

    make it better

--- src/controller/patientController.js
+++ src/controller/patientController.js
@@ -171,13 +171,13 @@ const gameHistory = async (req,res)=>
         //     return res.status(200).send({status : true , data : doctorID.progress})
         // }         
         // const Docid = req.query._id;
-        const patientid =req.query._id;
+        const patientId =req.query. _id;
 
         // if(!DocEmail){
         //     return res.status(400).send({status:true,message:"Please Enter doctor Email id"})
         // }
 
-        if(!patientid){
+        if(!patientId){
             return res.status(400).send({status:true,message:"Please Enter patient  id"})
         }
 
@@ -185,7 +185,7 @@ const gameHistory = async (req,res)=>
         //      return res.status(400).send({ status: false, message: "Doctor email is not valid" })
         // }
 
-        if (!(/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/.test(patientid))) {
+        if (!(/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/.test(patientId))) {
             return res.status(400).send({ status: false, message:"Patient email is not valid" })
        }
 
@@ -196,7 +196,7 @@ const gameHistory = async (req,res)=>
     //    }
      //  console.log("Doc", doctor._id)
 
-       const details = await patientModel.findOne({_id : patientid})
+       const details = await patientModel.findOne({_id : patientId})
        //console.log(details.DocId)
        if(!details){
         return res.status(404).send({status: false,message:"No such data found,Please chck email id"})
